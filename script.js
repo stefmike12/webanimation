5b4114902c866928bc88c5d0c1c0713fa5c83e6c
Mike van den Bos <stefmike12@gmail.com>
2019-05-18 20:47:43 +0200

    aanpassing

--- script.js
+++ script.js
@@ -1,30 +1,65 @@
-
 $(document).ready(function () {
-    TweenMax.to($('#oranje-lijn'), 5, { x: -250, ease: Linear.easeInOutQuint, yoyoEase:true, repeat:-1})
-    TweenMax.to($('#oranje-balk-boven'), 3, { x: 250, ease: Linear.easeInOutQuint, yoyoEase: true, repeat: -1 })
-    TweenMax.to($('#zwarte-balk-3'), 4, { x: 120, ease: Linear.easeInOutQuint, yoyoEase: true, repeat: -1 })
-    TweenMax.to($('#zwarte-balk-2'), 3, { x: 400, ease: Linear.easeInOutQuint, yoyoEase: true, repeat: -1 })
-    TweenMax.to($('#zwarte-balk-1'), 4, { x: -400, ease: Linear.easeInOutQuint, yoyoEase: true, repeat: -1 })
-    TweenMax.to($('#oranje-balk-onder'), 2, { x: -200, ease: Linear.easeInOutQuint, yoyoEase: true, repeat: -1 })
- });
+    TweenMax.to($('#oranje-lijn'), 5, {
+        x: -250,
+        ease: Linear.easeInOutQuint,
+        yoyoEase: true,
+        repeat: -1
+    })
+    TweenMax.to($('#oranje-balk-boven'), 3, {
+        x: 250,
+        ease: Linear.easeInOutQuint,
+        yoyoEase: true,
+        repeat: -1
+    })
+    TweenMax.to($('#zwarte-balk-3'), 4, {
+        x: 120,
+        ease: Linear.easeInOutQuint,
+        yoyoEase: true,
+        repeat: -1
+    })
+    TweenMax.to($('#zwarte-balk-2'), 3, {
+        x: 400,
+        ease: Linear.easeInOutQuint,
+        yoyoEase: true,
+        repeat: -1
+    })
+    TweenMax.to($('#zwarte-balk-1'), 4, {
+        x: -400,
+        ease: Linear.easeInOutQuint,
+        yoyoEase: true,
+        repeat: -1
+    })
+    TweenMax.to($('#oranje-balk-onder'), 2, {
+        x: -200,
+        ease: Linear.easeInOutQuint,
+        yoyoEase: true,
+        repeat: -1
+    })
+});
 
 
 
-function changeOverlay () {
+function changeOverlay() {
     var randomNum = Math.round(Math.random() * 150) - 8;
-    $('#Artboard-Copy path:eq(' + randomNum + ')').animate({ "opacity" : 0 });
-    setTimeout(function() { 
-        $('#Artboard-Copy path:eq(' + randomNum + ')').animate({ "opacity" : 1 });
+    $('#Artboard-Copy path:eq(' + randomNum + ')').animate({
+        "opacity": 0
+    });
+    setTimeout(function () {
+        $('#Artboard-Copy path:eq(' + randomNum + ')').animate({
+            "opacity": 1
+        });
         changeOverlay();
     }, 200);
-	    setTimeout(function() { 
-        $('#Artboard-Copy path:eq(' + randomNum + ')').animate({ "opacity" : 1 });
+    setTimeout(function () {
+        $('#Artboard-Copy path:eq(' + randomNum + ')').animate({
+            "opacity": 1
+        });
         // setTimeout();
     }, 200);
-	
+
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
     changeOverlay();
 });
 
@@ -33,22 +68,22 @@ var hueRotate = 0;
 var toggle = false;
 
 function onButtonClick() {
-  var body = document.getElementsByTagName('body')[0];
-  toggle = !toggle;
-  var css = toggle ? 'filter: invert(100%)' : 'filter: invert(0%)';
-  css += 'hue-rotate(' + hueRotate + 'deg);';
-  body.style = css;
+    var body = document.getElementsByTagName('body')[0];
+    toggle = !toggle;
+    var css = toggle ? 'filter: invert(100%)' : 'filter: invert(0%)';
+    css += 'hue-rotate(' + hueRotate + 'deg);';
+    body.style = css;
 }
 
 // Wanneer de pagina geladen is en alle elementen er zijn
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Wanneer de gebruiker zijn muiswiel beweegt
-    $(document).on('mousewheel', function() {
+    $(document).on('mousewheel', function () {
         // Voeg dan de CSS animation property aand de .door-left en -right toe
         $('.door-left, .door-right').css('WebkitAnimation', 'doors 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards')
     });
-    
+
     // Wanneer de gebruiker klikt
     $(document).on('click', function () {
         // Voeg dan de CSS animation property aand de .door-left en -right toe
@@ -100,10 +135,10 @@ $(document).ready(function() {
         var css = 'hue-rotate(' + value + 'deg) ';
         // Voeg de invert hier aan toe
         css += toggle ? 'invert(100%)' : 'invert(0%)';
-        
+
         // en geef de body deze CSS
         $('body').css('filter', css);
-    })
+    });
 
     $('.glitch').draggable({
         grid: [40, 40],
