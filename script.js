1f4da0c7939753bbcd29f537c347c00189446863
Mike van den Bos <stefmike12@gmail.com>
2019-05-18 21:01:57 +0200

    aanpassingen

--- script.js
+++ script.js
@@ -79,40 +79,40 @@ function onButtonClick() {
 $(document).ready(function () {
 
     // Wanneer de gebruiker zijn muiswiel beweegt
-    $(document).on('mousewheel', function () {
+    $(document).on("mousewheel", function () {
         // Voeg dan de CSS animation property aand de .door-left en -right toe
-        $('.door-left, .door-right').css('WebkitAnimation', 'doors 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards')
+        $(".door-left, .door-right").css("WebkitAnimation", "doors 1000ms cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards")
     });
 
     // Wanneer de gebruiker klikt
-    $(document).on('click', function () {
+    $(document).on("click", function () {
         // Voeg dan de CSS animation property aand de .door-left en -right toe
-        $('.door-left, .door-right').css('WebkitAnimation', 'doors 2s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards')
+        $(".door-left, .door-right").css("WebkitAnimation", "doors 1000ms cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards")
     });
 
     // Wanneer de gebruiker scrollt
-    $(document).on('mousewheel', function () {
+    $(document).on("mousewheel", function () {
         // Voeg dan de CSS animation property aand de .scroll titels scroll up
-        $('.scroll').css('WebkitAnimation', 'scroll 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards')
-        $('.up').css('WebkitAnimation', 'up 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards')
+        $(".scroll").css("WebkitAnimation", "scroll 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards")
+        $(".up").css("WebkitAnimation", "up 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards")
         setTimeout(function () {
-            $('.scroll').css('display', 'none');
+            $(".scroll").css("display", "none");
         }, 1000);
         setTimeout(function () {
-            $('.up').css('display', 'none');
+            $(".up").css("display", "none");
         }, 1000);
     });
 
     // Wanneer de gebruiker klikt
-    $(document).on('click', function () {
+    $(document).on("click", function () {
         // Voeg dan de CSS animation property aand de .scroll titels scroll up
-        $('.scroll').css('WebkitAnimation', 'scroll 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards')
-        $('.up').css('WebkitAnimation', 'up 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards')
+        $(".scroll").css("WebkitAnimation", "scroll 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards")
+        $(".up").css("WebkitAnimation", "up 1s cubic-bezier(0.25, 0.46, 0.45, 0.94) forwards")
         setTimeout(function () {
-            $('.scroll').css('display', 'none');
+            $(".scroll").css("display", "none");
         }, 1000);
         setTimeout(function () {
-            $('.up').css('display', 'none');
+            $(".up").css("display", "none");
         }, 1000);
     });
 
